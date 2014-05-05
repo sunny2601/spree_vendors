61e94cbff500e487380858d7dacc5af0096d7876
David Ang <davidang09@gmail.com>
2014-05-05 09:58:23 +0800

    can now delete vendor images and uses a date widget

--- app/controllers/spree/admin/vendors_controller.rb
+++ app/controllers/spree/admin/vendors_controller.rb
@@ -8,9 +8,15 @@ module Spree
       end
 
       def edit
-        @vendor = Vendor.find(params[:id])
+        #@vendor = Vendor.find(params[:id])
         @vendor.vendor_images.build
       end 
+
+      def delete_vendor_image
+        @vendor.vendor_images.where(id: params[:image]).first.destroy
+        flash[:notice] = "The image was successfully deleted"
+        redirect_to edit_admin_vendor_url(@vendor)
+      end
 
     end
   end
