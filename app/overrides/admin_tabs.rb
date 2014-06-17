2a258cfd2262ae6f64bd656ce55e4c570936b832
Bryan Mahoney <bryan@goDynamo.com>
2014-06-17 12:57:48 -0400

    Updates vendor icon

--- app/overrides/admin_tabs.rb
+++ app/overrides/admin_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "vendors_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:vendors, :icon => 'icon-tags') %>")
+                     :text => "<%= tab(:vendors, :icon => 'truck') %>")
 
